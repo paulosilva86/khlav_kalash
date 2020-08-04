4d9e85dc247076f9203a54b17117b27961ba4f78
Paulo Silva <paulosilva86@gmail.com>
2020-08-05 00:18:41 +0100

    Add system specs for creating an Order
    Why:
    
    * So we can test creating an Order end-to-end.
    
    This change addresses the need by:
    
    * Adding system specs for creating an Order successfully.
    * Adding system specs for creating an Order with errors.

---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Orders", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it "allows the user to create an order successfully" do
+    visit "/orders/new"
+
+    fill_in "First name", :with => "User"
+    fill_in "Postal code", :with => "35001"
+    fill_in "Email address", :with => "user@example.com"
+
+    click_button "Pay $2.99"
+
+    expect(page).to have_text("Order was successfully created.")
+  end
+
+  it "does not allow the user to create an order successfully" do
+    visit "/orders/new"
+
+    click_button "Pay $2.99"
+
+    expect(page).to have_text("errors prohibited this order from being saved")
+  end
+end
